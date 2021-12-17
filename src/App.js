3f5c1ec9c0c4b7e504215431bf4b17dd0849df53
Ben <bzilber@users.noreply.github.com>
2021-12-17 17:09:47 -0500

    removeChar added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Table from './Table'
 
 class App extends Component {
+ 
   state = {
      characters: [
       {
@@ -22,11 +23,23 @@ class App extends Component {
       }
      ],
   }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+
+    this.setState({
+      characters: characters.filter((character,i) => {
+        return i != index
+      }),
+    })
+  }
+
     render() {
-        
+        const {characters} = this.state
+
         return (
             <div className="container">
-                <Table characterData = {characters}/>
+                <Table characterData = {characters} removeCharacter={this.removeCharacter} />
             </div>
         )
     }
