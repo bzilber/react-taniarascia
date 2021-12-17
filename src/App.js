996afdd90008d39641248ebaa05a838a258abe89
Ben <bzilber@users.noreply.github.com>
2021-12-17 14:07:28 -0500

    React Tutorial and template

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import Table from './Table'
+
+class App extends Component {
+  state = {
+     characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      }
+     ],
+  }
+    render() {
+        
+        return (
+            <div className="container">
+                <Table characterData = {characters}/>
+            </div>
+        )
+    }
+}
+
+export default App
